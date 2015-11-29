240c273d3c680ef7e33713f7ccc50024f2cc0a87
Macioza <windsurfingpoland@gmail.com>
2015-11-30 00:23:32 +0100

    Update and rename Main to test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+from ubidots import ApiClient
+import random
+import time
+
+api = ApiClient("92c3631d495bfff93d3d3cf043772d46003d9e5b")
+test_variable = api.get_variable("565b6ef5762542203c6a4610")
+
+def main():
+	while True:
+		test_value = random.randint(1,100)
+		test_variable.save_value({'value':test_value})
+		time.sleep(10)
+
+try:
+	main()
+except:
+	main()
