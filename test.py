b0af3ae0275011952dd81eb401acc76e2f385177
Macioza <windsurfingpoland@gmail.com>
2015-11-30 00:46:51 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,21 +2,15 @@ from ubidots import ApiClient
 import random
 import time
 
-def polacz_z_ubidots():
-	try:
-		api = ApiClient("92c3631d495bfff93d3d3cf043772d46003d9e5b")
-		test_variable = api.get_variable("565b6ef5762542203c6a4610")
-	except:
-		print "Connection error, trying again"
-		time.sleep(40)
-		api = ApiClient("92c3631d495bfff93d3d3cf043772d46003d9e5b")
-		test_variable = api.get_variable("565b6ef5762542203c6a4610")
+api = ApiClient("92c3631d495bfff93d3d3cf043772d46003d9e5b")
+test_variable = api.get_variable("565b6ef5762542203c6a4610")
 
 def main():
 	while True:
 		test_value = random.randint(1,100)
-		test_variable.save_value({'value':test_value})
-		time.sleep(1)
-
-polacz_z_ubidots()
-main()
+		try:
+			test_variable.save_value({'value':test_value})
+		except:
+			time.sleep(40)
+			test_variable.save_value({'value':test_value})
+		time.sleep(10)
