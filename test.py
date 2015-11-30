54868b959a0e1ca058de3db7d99f38a80d0af0b1
Macioza <Macioza@MacBook-Pro-Maciej.local>
2015-11-30 12:10:48 +0100

    e

--- test.py
+++ test.py
@@ -10,7 +10,9 @@ def main():
 		test_value = random.randint(1,100)
 		try:
 			test_variable.save_value({'value':test_value})
-			print "success"
+			x = 0
+			print "success" x
+			x = x+1
 		except:
 			print "error"
 			time.sleep(40)
