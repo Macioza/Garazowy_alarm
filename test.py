bccb0d699cf44e56ac5b413951154485edf25bc5
Macioza <Macioza@MacBook-Pro-Maciej.local>
2015-11-30 12:34:28 +0100

    licznik 3

--- test.py
+++ test.py
@@ -6,13 +6,13 @@ api = ApiClient("92c3631d495bfff93d3d3cf043772d46003d9e5b")
 test_variable = api.get_variable("565b6ef5762542203c6a4610")
 
 def main():
+	licznik = 0
 	while True:
 		test_value = random.randint(1,100)
 		try:
 			test_variable.save_value({'value':test_value})
-			licznik = 0
 			print "success"
-			licznik = licznik+1
+			licznik = licznik + 1
 			print (licznik)
 		except:
 			print "error"
