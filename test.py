75ded593c43f934d5fb58f01f1e412ead37fe1e2
Macioza <Macioza@MacBook-Pro-Maciej.local>
2015-11-30 14:38:44 +0100

    up

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from ubidots import ApiClient
 import random
 import time
-import requests
+#import requests
 
 api = ApiClient("92c3631d495bfff93d3d3cf043772d46003d9e5b")
 test_variable = api.get_variable("565c48007625425dd74d15c7")
