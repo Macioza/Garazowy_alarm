882f23a764e9c2a263bba3e04a624cadbe0202d9
Macioza <Macioza@MacBook-Pro-Maciej.local>
2015-12-10 00:02:22 +0100

    Pierwsza iteracja
    Pierwsze i najważniejsze: program nie jest już nieskończoną pętlą oraz
    nie łamie go brak połączenia z internetem.
    Zintegrowałem żądania do IFTTT z powiadomieniami Push. Badam
    opóźnienie.

--- hell.py
+++ hell.py
@@ -1,14 +1,21 @@
 import requests
 import time
-def polacz(): 
-	status = requests.get("https://maker.ifttt.com/trigger/1/with/key/RpT2e05KGbf-5B4jO00o7")
-	#print(status.text)
-	while status.text == "Congratulations! You've fired the 1 event":
-		try:
-			requests.post("https://maker.ifttt.com/trigger/1/with/key/RpT2e05KGbf-5B4jO00o7")
-			print("ok")
-		except:
-			print("error")
+def polacz():
+	try:
+		requests.post("https://maker.ifttt.com/trigger/1/with/key/RpT2e05KGbf-5B4jO00o7")
+		print("ok")
+		time.sleep(5)
+	except:
+		print("not ok")
+		time.sleep(5)
 
-polacz()
-
+if input("to start, press one ") == 1:
+	try:
+		i = 0
+		status = requests.get("https://maker.ifttt.com/trigger/1/with/key/RpT2e05KGbf-5B4jO00o7")
+		while status.text == "Congratulations! You've fired the 1 event" and i <= 3:
+			i = i + 1
+			polacz()
+			print(i)
+	except:
+		print("chuj")
