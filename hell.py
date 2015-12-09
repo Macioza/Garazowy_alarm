87231f4aafe7270f7b0b2e7b851eeb846945d8a4
Macioza <Macioza@MacBook-Pro-Maciej.local>
2015-12-10 00:13:03 +0100

    2 x pytanie
    Dodałem dwa zapytania ułatwiające testowanie aplikacji.

--- hell.py
+++ hell.py
@@ -4,16 +4,18 @@ def polacz():
 	try:
 		requests.post("https://maker.ifttt.com/trigger/1/with/key/RpT2e05KGbf-5B4jO00o7")
 		print("ok")
-		time.sleep(5)
+		time.sleep(wait)
 	except:
 		print("not ok")
-		time.sleep(5)
+		time.sleep(wait)
 
 if input("to start, press one ") == 1:
+	connections = input("select number of connections ")
+	wait = input("specyfy interval ")
 	try:
 		i = 0
 		status = requests.get("https://maker.ifttt.com/trigger/1/with/key/RpT2e05KGbf-5B4jO00o7")
-		while status.text == "Congratulations! You've fired the 1 event" and i <= 3:
+		while status.text == "Congratulations! You've fired the 1 event" and i <= connections:
 			i = i + 1
 			polacz()
 			print(i)
