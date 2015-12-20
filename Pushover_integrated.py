12569ef350bd487744760f1563d65d21103e9c7c
Macioza <Macioza@MacBook-Pro-Maciej.local>
2015-12-21 00:30:25 +0100

    Nieskończona pętla #1
    Kod jest odporny na zerwanie połączenia z internetem. Po uruchomieniu
    wysyła powiadomienia aż do ctrl + z

--- Pushover_integrated.py
+++ Pushover_integrated.py
@@ -1,25 +1,25 @@
-import httplib, urllib
-import urllib2
+import httplib, urllib, urllib2, time
 
 def internet_on():
-    try:
-        response=urllib2.urlopen('https://www.google.pl',timeout=1)
-        return True
-    except urllib2.URLError as err: pass
-    return False
+	try:
+		esponse=urllib2.urlopen('https://www.google.pl',timeout=1)
+		return True
+	except: 
+		pass
+		return False
 print(internet_on())
-x = internet_on()
-if x == True: 
-	conn = httplib.HTTPSConnection("api.pushover.net:443") #determinuje conn jako obiekt 	klasy HTTPSConnection
-	conn.request("POST", "/1/messages.json",
-             # wywoluje funcje request z klasy HTTPSConnection.
-             # POST to metoda,
-             # "1/messages.json" to chyba url,
-             # urllib.urlencode to body, a Content-type to header.
-             # Funkcja request przyjmuje cztery zmienne, urllib.urlencode to stanowi jedna z nich.
-	urllib.urlencode({
-		"token": "aMrBqbkbRRgjrEUn5HnDvJrikTnViT",
-		"user": "ucyKVK6zEZ5LKe9kmS2cgPRgwvAyF3",
-		"message": "wiem, ze mnie lubisz",
-		"sound": "siren" }), { "Content-type": "application/x-www-form-urlencoded" })
-	print(conn.getresponse())
+
+while True:
+	try:
+		if internet_on() == True:
+			conn = httplib.HTTPSConnection("api.pushover.net:443")
+			conn.request("POST", "/1/messages.json",
+				urllib.urlencode({
+					"token": "aMrBqbkbRRgjrEUn5HnDvJrikTnViT",
+					"user": "ucyKVK6zEZ5LKe9kmS2cgPRgwvAyF3",
+					"message": "wiem, ze mnie lubisz",
+					"sound": "siren" }), { "Content-type": "application/x-www-form-urlencoded" })
+		print(conn.getresponse())
+		time.sleep(5)
+	except:
+		pass
