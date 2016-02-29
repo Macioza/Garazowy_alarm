a430d2f47517dcd6f72ec444d19309cead3e568b
Macioza <windsurfingpoland@gmail.com>
2016-02-29 22:03:30 +0100

    Dodana dioda
    Dodałem diodę pod pinem 13.

--- arduino.ino
+++ arduino.ino
@@ -5,14 +5,17 @@ int status1 = 0;
 void setup() {
 pinMode(brama1, INPUT);
 pinMode(brama1out, OUTPUT);
+pinMode(13, OUTPUT);
 }
 
 void loop() {
   status1 = digitalRead(brama1);
   if (status1 == LOW) {
     digitalWrite(brama1out, LOW);
+    digitalWrite(13, HIGH);
   }
   else {
     digitalWrite(brama1out, HIGH);
+    digitalWrite(13, LOW);
   }
 }
