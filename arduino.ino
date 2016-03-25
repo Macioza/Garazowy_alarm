2d0e152dc9401677a81aa8e00a369316ec9ab3b2
Macioza <windsurfingpoland@gmail.com>
2016-03-25 13:55:19 +0100

    Drugi kontaktron
    W końcu podłączyłem drugi kontaktron. Arduino łączy się przez konwerter stanów logicznych. Na razie wykorzystane są dwa kable (biały i zielony). Wydaje mi się, że demontując poprzedni prototyp uszkodziłem konwerter dlatego pin L2 (czyli różowy kabel) nie działa prawidłowo. Stan niski to tylko 3.0V, w przeciwieństwie do prawidłowo działających pinów - 0.2 V. Nie mniej jednak RPi widzi oba stany i wysyła odpowiednie powiadomienia Pushover w zależności od tego, który kontaktron jest rozwarty.

--- arduino.ino
+++ arduino.ino
@@ -1,21 +1,37 @@
 const int brama1 = 4;
-const int brama1out = 2;
+const int brama2 = 5;
+const int bialy = 2;
+const int zielony = 3;
 int status1 = 0;
+int status2 = 0;
 
 void setup() {
 pinMode(brama1, INPUT);
-pinMode(brama1out, OUTPUT);
+pinMode(brama2, INPUT);
+pinMode(bialy, OUTPUT);
+pinMode(zielony, OUTPUT);
 pinMode(13, OUTPUT);
 }
 
 void loop() {
   status1 = digitalRead(brama1);
+  status2 = digitalRead(brama2);
+  
   if (status1 == LOW) {
-    digitalWrite(brama1out, LOW);
+    digitalWrite(bialy, LOW);
     digitalWrite(13, HIGH);
   }
   else {
-    digitalWrite(brama1out, HIGH);
+    digitalWrite(bialy, HIGH);
+    digitalWrite(13, LOW);
+  }
+
+  if (status2 == LOW) {
+    digitalWrite(zielony, LOW);
+    digitalWrite(13, HIGH);
+  }
+  else {
+    digitalWrite(zielony, HIGH);
     digitalWrite(13, LOW);
   }
 }
